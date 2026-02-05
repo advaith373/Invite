2d4481eab28e095b1fee2453aa1d12d080fdd2a8
advaith373 <advaithshetty33@gmail.com>
2026-02-05 14:13:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const ACCESS_CODE = "INITIATE";
+
+function checkCode() {
+  const input = document.getElementById("codeInput").value;
+  const error = document.getElementById("errorMsg");
+
+  if (input.toUpperCase() === ACCESS_CODE) {
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("invite").classList.remove("hidden");
+
+    const audio = document.getElementById("accessAudio");
+    audio.play();
+  } else {
+    error.textContent = "Invalid access code.";
+  }
+}
